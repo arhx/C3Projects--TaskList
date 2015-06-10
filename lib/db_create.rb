9a6f811753e4d0589fd159d84f6bafb68316f219
Brenna <brennaleker@gmail.com>
2015-06-10 15:16:59 -0700

    setup db_create to create our database

---
+++ lib/db_create.rb
@@ -0,0 +1,10 @@
+require 'sqlite3'
+
+db = SQLite3::Database.new "db/tasklist.db"
+db.execute "CREATE TABLE tasks
+  (id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT,
+  completed_date TEXT);"
+
+db.close if db
